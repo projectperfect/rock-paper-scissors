4f4fac46f649911d0b1b6c024f58e5acc77430fc
projectperfect <jprfct@protonmail.com>
2022-07-27 21:53:41 -0700

    computer choice added.

--- main.py
+++ main.py
@@ -31,3 +31,7 @@ game_images = [rock, paper, scissors]
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
